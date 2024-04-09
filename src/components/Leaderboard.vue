cc50db4a53d7e4133d1592b43cdee7fd0530970c
HLMelton <hunterlmelton@gmail.com>
2024-04-08 18:21:22 -0700

    Leaderboard minor fix

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="overflow-x-auto">
-  <table class="table">
+  <table class="table-md">
     <!-- head -->
     <thead>
       <tr>
@@ -10,7 +10,6 @@
         <th>Region/Blend</th>
         <th>Process</th>
         <th>Upvote</th>
-        <th>Coffeedex Rank</th>
         <th>Downvote</th>
       </tr>
     </thead>
@@ -25,13 +24,20 @@
         <td>Washed</td>
         <th>
           <label>
-            <button class="btn">Button</button>
+            <button class="btn">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+              </svg>
+            </button>
           </label>
         </th>
-        <td>0</td>
         <th>
           <label>
-            <button class="btn">Button</button>
+            <button class="btn">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+              </svg>
+            </button>
           </label>
         </th>
       </tr>
@@ -44,15 +50,22 @@
         <td>Ethiopia</td>
         <td>Anerobic Natural</td>
         <th>
-          <label>
-            <button class="btn">Button</button>
-          </label>
-        </th>
-        <td>0</td>
-        <th>
-          <label>
-            <button class="btn">Button</button>
-          </label>
+        <label>
+          <button class="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+            </svg>
+          </button>
+        </label>
+      </th>
+      <th>
+        <label>
+          <button class="btn">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+          </button>
+        </label>
         </th>
       </tr>
 
